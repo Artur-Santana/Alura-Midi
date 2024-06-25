b49b7a8e6e1a8342d95d3ea2785462bc5e9ade72
Artur Santana <artursantana150@gmail.com>
2024-06-24 21:47:56 -0300

    Aula 5.2 - Refatorar função tocaSom para melhorar legibilidade e tratamento de erros

--- js/main.js
+++ js/main.js
@@ -1,6 +1,17 @@
 
-function tocaSom (idElementoAudio) {
-  document.querySelector(idElementoAudio).play();
+function tocaSom (seletorAudio) {
+  const elemento = document.querySelector(seletorAudio);
+
+  if (elemento === null){
+    console.log('Elemento não encontrado')
+  }
+  if (elemento != null){
+    
+    if (elemento.localName === 'audio'){
+      elemento.play()
+    }
+    
+  }
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
@@ -16,8 +27,11 @@ for (let contador = 0; contador < listaDeTeclas.length; contador++) {
 
     tecla.onkeydown =  function (evento) {
 
-      if (evento.code === 'Space'){
-        tecla.classList.add('ativa');}
+      if (evento.code === 'Enter' || evento.code === 'Space') {
+        tecla.classList.add('ativa');
+      }
+
+
     }
     
     tecla.onkeyup = function() {
